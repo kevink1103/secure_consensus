83f10e9c1515e569f5438b15afd86807981b0741
Kevin Kim <kevink1103@gmail.com>
2019-10-16 14:31:28 +0800

    minor

--- noise.py
+++ noise.py
@@ -1,3 +1,7 @@
+# Privacy Preserving Average Consensus
+# by Yilin Mo, Member, IEEE, Richard M. Murray, Fellow, IEEE
+# DOI 10.1109/TAC.2016.2564339, IEEE Transactions on Automatic Control
+
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,37 +11,39 @@ from algorithm import Algorithm
 
 class NoiseAlgo(Algorithm):
     def __init__(self):
+        # Based on VI. NUMERICAL EXAMPLES
         self.A = (1 / 4) * np.matrix([
-                    [2, 1, 0, 0, 1],
-                    [1, 2, 1, 0, 0],
-                    [0, 1, 2, 0, 1],
-                    [0, 0, 0, 3, 1],
-                    [1, 0, 1, 1, 1]
-                ])
+            [2, 1, 0, 0, 1],
+            [1, 2, 1, 0, 0],
+            [0, 1, 2, 0, 1],
+            [0, 0, 0, 3, 1],
+            [1, 0, 1, 1, 1]
+        ])
         self.phi = 0.9
-        self.agents = [-1.4, -0.8, 1.2, 0.7, -0.5]
-        self.agents_history = []
-        self.noises = []
-        self.init_avg = self.__average(self.agents)
+        self.agents = [-1.4, -0.8, 1.2, 0.7, -0.5] # by myself
         self.time = 50
+        self.__agents_history = []
+        self.__noises = []
+        self.__init_avg = self.__average(self.agents)
 
     def run(self):
-        for k in range(self.time):
+        # Algorithm from III. PROBLEM FORMULATION
+        for k in range(self.time): # this is step 4
             self.__step1(k)
             self.__step2(k)
             self.__step3(k)
-        prnt(self.agents_history)
+        prnt(self.__agents_history, both=True)
 
     def plot(self):
         # reorganize
-        ys = [[] for i in range(len(self.agents_history[0]))]
-        for xs in self.agents_history:
+        ys = [[] for i in range(len(self.__agents_history[0]))]
+        for xs in self.__agents_history:
             for i, x in enumerate(xs):
                 ys[i].append(float(x))
         # error vector
-        zs = [[] for i in range(len(self.agents_history[0]))]
+        zs = [[] for i in range(len(self.__agents_history[0]))]
         for i, y in enumerate(ys):
-            z = np.array(y) - np.array(self.init_avg)
+            z = np.array(y) - np.array(self.__init_avg)
             zs[i] = z.tolist()
 
         # plot
@@ -48,7 +54,7 @@ class NoiseAlgo(Algorithm):
         x = np.arange(0, self.time, 1)
         for i, y in enumerate(ys):
             plt.plot(x, y, label='x{}(k)'.format(i+1))
-        plt.axhline(y=self.init_avg, color='k', linestyle='dashed')
+        plt.axhline(y=self.__init_avg, color='k', linestyle='dashed')
         plt.xlabel('k')
         plt.ylabel('xi(k)')
         plt.xlim([0, self.time])
@@ -80,20 +86,18 @@ class NoiseAlgo(Algorithm):
 
     def __step1(self, k):
         noises = self.__random(len(self.agents))
-        self.noises.append(noises)
+        self.__noises.append(noises)
 
     def __step2(self, k):
         if k == 0:
-            self.noises[0] = self.noises[0]
+            self.__noises[0] = self.__noises[0]
         else:
-            self.noises[k] = ((self.phi ** k) * self.noises[k]) - ((self.phi ** (k-1)) * self.noises[k-1])
-
-        self.agents += self.noises[k]
+            self.__noises[k] = ((self.phi ** k) * self.__noises[k]) - ((self.phi ** (k-1)) * self.__noises[k-1])
+        self.agents += self.__noises[k]
     
     def __step3(self, k):
         reshaped_agents = np.reshape(np.array(self.agents), (5, 1))
         reshaped_agents = self.A * reshaped_agents
         reshaped_agents = np.reshape(reshaped_agents, (1, 5)).tolist()[0]
         self.agents = reshaped_agents
-        self.agents_history.append(reshaped_agents)
-    
+        self.__agents_history.append(reshaped_agents)
